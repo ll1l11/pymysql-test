c34cbc040a7369ed45d6781ae11d927748406d47
codeif <me@codeif.com>
2017-02-09 16:22:22 +0800

    ext.py add logger

--- demo/ext.py
+++ demo/ext.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
+from werkzeug.local import LocalProxy
+from flask import current_app
 from flask_sqlalchemy import SQLAlchemy
 
 
+logger = LocalProxy(lambda: current_app.logger)
 db = SQLAlchemy()
